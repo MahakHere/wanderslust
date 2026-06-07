353582618fcec675dc48d6692e208ffd6a8d4cc2
MahakHere <mahakagrawal018@gmail.com>
2026-06-08 00:42:46 +0530

    Style navbar, footer, index and new listing page

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")))
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 //connect with databse
 main().then(() => {
