e97c122eb9123227ad1ca86deba815b98a529f5c
MahakHere <mahakagrawal018@gmail.com>
2026-06-13 10:40:02 +0530

    client side validation, server side validation,and error handling

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require("path");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const listingScema = require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -12,6 +15,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")))
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
+
+const validateListing = (req,res,next)=>{
+        let result = listingSchema.validate(req.body);
+        console.log(result.error)
+        if(result.error){
+            throw new ExpressError(400,result.error);
+        }else{
+            next();
+        }
+}
 
 //connect with databse
 main().then(() => {
@@ -29,77 +42,64 @@ app.get("/",(req,res)=>{
 })
 
 //index route
-app.get("/listings", (req, res) => {
-    Listing.find({}).then((result) => {
-        // console.log(result)
-        res.render("listings/index.ejs", { result })
-    }).catch((err) => {
-        console.log(err)
-        res.send("Some error occured!")
-    })
-})
+app.get("/listings", wrapAsync(async(req, res,next) => {
+    let result = await Listing.find({})
+    res.render("listings/index.ejs", { result })
+}));
 
 //new route
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 
-//create route
-app.post("/listings", (req, res) => {
-    let newListing = req.body;
-    // console.log(newListing);
-    New = new Listing(newListing);
-    New.save().then(() => {
-        console.log("Inserted successfully");
-    })
-    res.redirect("/listings")
-});
+//create route --> review
+app.post("/listings",validateListing ,wrapAsync(async(req, res,next) => {
+        let newListing = req.body;
+        New = new Listing(newListing);
+        await New.save()
+        res.redirect("/listings")
+}));
 
 //show route
-app.get("/listings/:id", (req, res) => {
+app.get("/listings/:id", wrapAsync(async(req, res,next) => {
     let { id } = req.params;
-    Listing.findById(id).then((result) => {
-        // console.log(result)
-        res.render("listings/show.ejs", { result });
-    }).catch((err) => {
-        console.log(err)
-    })
-});
+    let result = await Listing.findById(id);
+    res.render("listings/show.ejs", { result });
+}));
 
 //edit route
-app.get("/listings/:id/edit", (req, res) => {
-    let { id } = req.params
-    Listing.findById(id).then((result) => {
-        // console.log(result)
-        res.render("listings/edit.ejs", { result })
-    }).catch((err) => {
-        console.log(err);
-    })
-});
+app.get("/listings/:id/edit", wrapAsync(async(req, res,next) => {
+    let { id } = req.params;
+    let result = await Listing.findById(id);
+    res.render("listings/edit.ejs", { result })
+}));
 
 //update route
-app.put("/listings/:id", (req, res) => {
+app.put("/listings/:id",validateListing, wrapAsync(async(req, res,next) => {
     let { newTitle, newDescription, newPrice, newImage, newLocation, newCountry } = req.body;
     let { id } = req.params
-    Listing.findByIdAndUpdate(id, { title: newTitle, description: newDescription, price: newPrice, image: newImage, location: newLocation, country: newCountry }, { new: true, runValidators: true })
-        .then(() => {
-            // console.log("Successfully updated");
-        }).catch((err) => {
-            console.log(err);
-        })
+    await Listing.findByIdAndUpdate(id, { title: newTitle, description: newDescription, price: newPrice, image: newImage, location: newLocation, country: newCountry }, { new: true, runValidators: true })
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //delete route
-app.delete("/listings/:id", (req, res) => {
+app.delete("/listings/:id", wrapAsync(async(req, res,next) => {
     let { id } = req.params;
     console.log(id);
-    Listing.findByIdAndDelete(id).then(() => {
-        console.log("successfully deleted");
-    }).catch((err) => {
-        console.log(err);
-    });
+    await Listing.findByIdAndDelete(id);
+    console.log("successfully deleted");
     res.redirect("/listings");
+}));
+
+//error handling for all non existing routes
+app.use((req,res,next)=>{
+    next( new ExpressError(404,"Page not Found!"));
+});
+
+//error handling middleware
+app.use((err,req,res,next)=>{
+    let {status = 500,message} = err;
+    res.status(status).render("error.ejs",{status,message});
 });
 
 //running the server
